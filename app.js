1acabd8b9a85e2ee49bfdee38d94f74bd4cc81ae
weaponwizardscom <contact@weapon-wizards.com>
2025-06-30 18:45:17 +0200

    1

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     let currentSvg=null;
     const TEXTURE ="img/glock17.png";
     const MODELS={glock:"g17.svg",sig:"sig.svg",cz:"cz.svg"};
-const BG      =["img/t1.png","img/t2.png","img/t3.png","img/t4.png","img/t5.png","img/t6.png","img/t7.png"];
+let BG = ["img/t1.png","img/t2.png","img/t3.png","img/t4.png","img/t5.png","img/t6.png","img/t7.png"];  // default backgrounds
+const BG_DEFAULT = BG.slice();
+const BG_CZ = ["img/cz1.png","img/cz2.png","img/cz3.png","img/cz4.png"];
     
     const PRICE={zamek:400,szkielet:400,spust:150,lufa:200,zerdz:50,pazur:50,
                  zrzut:50,blokadap:50,blokada2:50,pin:50,stopka:150}; // płytka = 0
@@ -307,7 +309,15 @@ function chooseModel(model){
   const overlay=document.getElementById("model-select");
   if(overlay)overlay.classList.add("hidden");
   currentSvg=MODELS[model]||"g17.svg";
-  loadSvg();
+  
+  if(model==="cz"){
+    BG = BG_CZ;
+  } else {
+    BG = BG_DEFAULT;
+  }
+  bgIdx = 0;
+  changeBg();
+loadSvg();
 }
 
     async function sendMail(){
